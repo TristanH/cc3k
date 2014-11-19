5ff768fcb1297c18c7dbb5216b9b99290318a9d0
Sebastian Winemaker <winemaker@v1020-wn-187-207.campus-dynamic.uwaterloo.ca>
2014-11-19 18:07:21 -0500

    did a tiny bit of stuff

--- Goblin.h
+++ Goblin.h
@@ -1,4 +1,11 @@
 #ifndef __GOBLIN_H__
 #define __GOBLIN_H__
+#include "Player.h"
+    
+class Goblin : public Player {
+
+    public:
+
+};
 
 #endif
--- Vampire.h
+++ Vampire.h
@@ -1,4 +1,9 @@
 #ifndef __VAMPIRE_H__
 #define __VAMPIRE_H__
+#include "Enemy.h"
+
+class Vampire : public Enemy {
+
+};
 
 #endif
